302536b8013f9433e561ec98120d6351c67d1c39
Ezequiel <lastrenada@gmail.com>
2023-10-03 22:00:53 -0300

    Desafío de código: Crear variables

--- Alvarez_Ezequiel_JS_CrearVariables.js
+++ Alvarez_Ezequiel_JS_CrearVariables.js
@@ -1,5 +1,5 @@
-var altura; //Altura expresada en metros de la persona, la cual deberá ser igual a 1 metro o más. 
-var edad; // Edad expresada en años de la persona, la cual deberá ser mayor a 10 años 
+var altura = 1.2; //Altura expresada en metros de la persona, la cual deberá ser igual a 1 metro o más. 
+var edad = 14; // Edad expresada en años de la persona, la cual deberá ser mayor a 10 años 
 
 
 
