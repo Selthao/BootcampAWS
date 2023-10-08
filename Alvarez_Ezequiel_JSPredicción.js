228d0bee28b372d033341196a0b7da41b04e73ff
Ezequiel <lastrenada@gmail.com>
2023-10-08 00:13:52 -0300

    Alvarez_Ezequiel_JSPredicción.js

--- Alvarez_Ezequiel_JSPredicción.js
+++ Alvarez_Ezequiel_JSPredicción.js
@@ -8,7 +8,6 @@ function myBirthYearFunc(birthYearInput){
         console.log("Nací en" + birthYearInput);
     }
     
-
 //La tercera y última función imprimirá en pantalla "¡Sumando números! num1 es: 10 num2 es: 20 30"
 function add(num1, num2){
     console.log("¡Sumando números!");
